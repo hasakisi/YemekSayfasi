38cdb179c0c1e1430c96c1bac20c6106b7d08402
hasakisi <tunayylz22@gmail.com>
2024-03-26 01:43:42 +0300

    7.videoya kadar

--- YemekSayfası/Kullanici.Master.cs
+++ YemekSayfası/Kullanici.Master.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,13 @@ namespace YemekSayfası
 {
     public partial class Site1 : System.Web.UI.MasterPage
     {
+        sqlbaglantisi conn = new sqlbaglantisi();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Kategoriler", conn.baglanti());
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataList1.DataSource = dr;
+            DataList1.DataBind();
         }
     }
 }
---
+++ YemekSayfası/sqlbaglantisi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+namespace YemekSayfası
+{
+    public class sqlbaglantisi
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection conn = new SqlConnection (@"Data Source=DESKTOP-RPF1DV4\SQLEXPRESS;Initial Catalog=yemektarifi;Integrated Security=True");
+            conn.Open ();
+            return conn;
+        }
+    }
+}
