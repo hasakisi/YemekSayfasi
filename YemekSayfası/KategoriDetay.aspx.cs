31168397a2bac92017d11d8ef96ba4b81bed36df
hasakisi <tunayylz22@gmail.com>
2024-04-04 11:11:22 +0300

    Yeni Fotoğraf eklendi

--- YemekSayfası/KategoriDetay.aspx.cs
+++ YemekSayfası/KategoriDetay.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,16 @@ namespace YemekSayfası
 {
     public partial class Kategoriler : System.Web.UI.Page
     {
+        sqlbaglantisi sql = new sqlbaglantisi();
+        string kategoriid = "";
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            kategoriid = Request.QueryString["Kategoriid"];
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Yemekler WHERE Kategoriid=@p1",sql.baglanti());
+            cmd.Parameters.AddWithValue("@p1", kategoriid);
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataList2.DataSource = dr;
+            DataList2.DataBind();
         }
     }
 }
